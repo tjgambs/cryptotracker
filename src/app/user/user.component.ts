c09a82f60042f75e5483e5623e9516b12b031c18
pkkuriboh144 <pkkuriboh144@gmail.com>
2018-05-15 18:51:56 -0500

    Auth0 user token access implemented

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../auth/auth.service';
 
 @Component({
   selector: 'app-user',
@@ -6,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  profile: any;
+
+  constructor(public auth: AuthService) { }
 
   ngOnInit() {
+    if (this.auth.userProfile) {
+      this.profile = this.auth.userProfile;
+    } else {
+      this.auth.getProfile((err, profile) => {
+        this.profile = profile;
+      });
+    }
   }
 
 }
