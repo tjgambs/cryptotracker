d7d627c2dd4ebe23d9e7aa255b6feb7828f77075
Tim Gamble <tjgambs@gmail.com>
2018-06-09 12:17:14 -0500

    Change back to local

--- src/app/auth/auth0-variables.ts
+++ src/app/auth/auth0-variables.ts
@@ -7,5 +7,5 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
   clientID: 'dR7f9tlR4JqeWEo5meV8NsxyF2di1YiA',
   domain: 'csc-csc.auth0.com',
-  callbackURL: 'http://cryptotracker-csc360.s3-website.us-east-2.amazonaws.com/dashboard'
+  callbackURL: 'http://localhost:4200/dashboard'
 };
